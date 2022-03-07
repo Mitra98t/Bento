8cb86fe60e9b6ac23093b81aaae024ca15b98380
Federico Miraglia <51447075+Mitra98t@users.noreply.github.com>
2022-03-07 20:25:18 +0100

    Added key

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: '', // Write here your API Key
+  weatherKey: 'c9395a8704049591bd87605063374a71', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
